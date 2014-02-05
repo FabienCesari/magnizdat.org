b3d64897e13002bea9cc058f702a4c4775610c57
magnizdat <fabien.cesari@gmail.com>
2014-02-05 14:34:09 -0800

    cleanup the repo from the twitter app code

--- app_tokens.php
+++
@@ -1,7 +0,0 @@
-<?php
-// OAuth tokens
-$consumer_key = '4eulAbzwM46jJ8X9P4BeSQ';
-$consumer_secret = '5UoGPicq3wGA9z4JZhFEZ3Dtuun9Kna5kVNHqYDt7c';
-$user_token = '78374928-VLrZUbQmceh401sJN0dAWyMh0Nakw5bzCkvM3vBMp';
-$user_secret = 'hFsscugi8ZCChRbWnMaArqtMQf5WpRJAuUBG5sz0wJkq2';
-?>
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <script type="text/javascript" src="processing-1.4.1.min.js"></script>
-    <script type="text/javascript" src="timbre.js"></script>
-    <script type="text/javascript" src="animation.js"></script>
+    <script type="text/javascript" src="js/parralax.js"></script>
     <!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->    
@@ -12,44 +10,6 @@
   </head>
   
   <body>    
-    <form action="post_tweet.php" method="post">
-      <input type="text" name="tweet"/>
-      <input type="submit" value="post" />
-    </form>       
-    <?php
-      
-    session_start();
-
-    $debug = true;
-    if($debug == true)
-      {
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
-      }
-
-      if(isset($_SESSION['tweets_array']))
-	{
-	  $tweets = $_SESSION['tweets_array'];
-	  unset($_SESSION['tweets_array']);
-	  $j_tweets = json_encode($tweets);	  	  
-    ?>
-    
-    <p>
-      <canvas id="canvas1" width="200" height="200"></canvas>
-    </p>    
-    <script id="script1" type="text/javascript">
-      
-      var json_tweets=<?php echo $j_tweets ?>; //Getting back the tweets array.
-      // Simple way to attach js code to the canvas is by using a function
-      
-      var canvas = document.getElementById("canvas1");
-      // attaching the sketchProc function to the canvas
-      var p = new Processing(canvas, sketchProc);
-      
-    </script>
-    
-    <?php
-	}  
-    ?>
+        
   </body>
 </html>
