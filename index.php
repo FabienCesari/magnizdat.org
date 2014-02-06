7a3794dd08ffb28773dd32214e11c16302978af2
magnizdat <fabien.cesari@gmail.com>
2014-02-05 17:27:49 -0800

    index structure and first css stylsheet.

--- index.php
+++ index.php
@@ -1,15 +1,108 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title>magnizdat.org</title>    
     <meta charset="utf-8" />
-    <script type="text/javascript" src="js/parralax.js"></script>
+    <meta name="description" content="Fabien Cesari's online CV and portfolio" />
+    <link rel="stylesheet" media="all" href="css/style.css" />
     <!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->    
-    <title>magnizdat.org</title>    
+	<![endif]-->        
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 
+    <script>!window.jQuery && document.write('<script src="/js/jquery-1.6.1.min.js"><\/script>')</script>
+    <script type="text/javascript" src="js/parralax.js"></script>
   </head>
   
   <body>    
-        
+    
+    <div id="wrapper">
+      <header id="branding">
+	<h1>Fabien Cesari</h1>
+      </header>
+      <nav id="primary">
+	<ul>
+	  <li>
+	    <h1>Signal processing engineer - developer</h1>
+	    <a class="work_exp" href="#work_exp">View</a>
+	  </li>
+	  <li>
+	    <h1>Education</h1>
+	    <a class="education" href="#education">View</a>
+	  </li>
+	  <li>
+	    <h1>Music</h1>
+	    <a class="music" href="#music">View</a>
+	  </li>
+	  <li>
+	    <h1>Purpose of this website</h1>
+	    <a class="about" href="#about">View</a>
+	  </li>
+	</ul>
+      </nav>            
+    </div>
+    
+    <div id="content">
+      <article id="work_exp">
+	<header>
+	  <h1>Signal processing engieer - developer</h1>
+	</header>
+	  <p>	    	  </p>
+	  <nav class="next-prev">
+	    <hr />
+	    <a class="next education" href="#education">Next</a>
+	  </nav>
+      </article>
+	
+	<article id="education">
+	  <header>
+	    <h1>education</h1>
+	  </header>
+	  <p>	  	    	  </p>	  
+	  <nav class="next-prev">
+	    <a class="prev work_exp" href="#work_exp">Prev</a>
+	    <hr />
+	    <a class="next music" href="#music">Next</a>
+	  </nav>
+	</article>
+	
+	<article id="music">
+	  <header>
+	    <h1>Music</h1>
+	  </header>
+	  <p>	    	  </p>
+	  <nav class="next-prev">
+	    <a class="prev education" href="#education">Prev</a>
+	    <hr />
+	    <a class="next about" href="#about">Next</a>
+	  </nav>
+	</article>
+	
+	<article id="about">
+	  <header>
+	    <h1>About</h1>
+	  </header>
+	  <p>	    	  </p>	  
+	  <nav class="next-prev">
+	    <a class="prev music" href="#music">Prev</a>
+	    <hr />
+	  </nav>
+	</article>
+      </div>
+    </div>
+    
+    <!-- Parralax for background -->
+    <div id="parralax_bg1">      
+      <!-- <img id="bg1-3" src="im/sky_ground.jpg" alt="cloud"/> -->
+    </div>
+    
+    <!-- Parralax for midground -->
+    <div id="parralax_bg2">      
+    </div>
+
+    <!-- Parralax for foreground -->
+    <div id="parralax_bg3">      
+    </div>
+    
+
   </body>
 </html>
