d4ff6dbc776cc5076b7e7a7fbab557e4d01a9621
magnizdat <fabien.cesari@gmail.com>
2014-02-28 15:05:13 -0800

    first step of style sheet, creation of the background animation canvas

--- js_version/index.php
+++ js_version/index.php
@@ -15,52 +15,35 @@
 	<h1>Fabien Cesari</h1>
 	<h2> Engineer, Musician, developer, digital craftsman.</h2>
       </header>
-      
-      <div id="content">
-	<article id="work_exp">
-	  <header>
-	    <h1>Where I come from.</h1>
-	  </header>
-	  <p>
-	    I am an engineer specialized in signal processing and electronic from <a href="http://www.ensea.fr"><i>l'Ecole Nationale Superieur de l'Electronique et de ses Applications (ENSEA)</i></a> in Paris, France. Since my graduation in 2011 I have been working as a developer for companies such as  <a href="http://www.voxler.fr/"> <i>Voxler Games </i></a> or <a href="http://www.arkamys.com/home.php"> <i>Arkamys</i></a> mostly n embeeded audio processing and voice regognition.For the past year I worked at <a href="http://www.ircam.fr/"> <i>L'Institut de Recherche et Coordination Acoustique / Musique (IRCAM) </i></a> as a research engineer and a <i>C++</i> developer. 
-	  </p>
-	  <p>
-	    I am also a musician and a sound passionate. I am currently working on multiple projects that involves technologie and sounds. 
-	  </p>
-	  
-	</article>
-	
-	<article id="education">
-	  <header>
-	    <h1>Where I am</h1>
-	  </header>
-	  <p>My current work is focused on designing tangible objects and electronic instruments that allows the composition and the improvisation without having to use a computer. I am also interrested with the development of web applications that allows real time interractions.
-	  </p>
-	  
-	</article>
-	
-	<article id="music">
-	  <header>
-	    <h1>Where I am going.</h1>
-	  </header>
-	  <p>
-	    I am currently looking for employments opportunities in San Francisco and in the bay area, I am eager to discover new technologies and increase the span of skills, and I am very open on the fields of applications. I worship interdisciplinarity and I think that creativity dosen't come from a spontaneous generation.
-	  </p>	
-	</article>
-	
-	<article id="about">
-	  <header>
-	    <h1>About</h1>	 
-	  </header>
-	  <p>	    	
-	    you can also find me <a href="http://www.twitter.com/magnizdat"><i>here</i></a>.
-	  </p> 
-	  <p>
-	    and a small part of my work can be seen <a href="http://www.github.com/FabienCesari"><i>here</i></a>.
-	  </p>	  
-	</article>
-      </div>
     </div>
-     
+
+    <div class="box">
+      <p class="text">	       
+	I am an engineer specialized in signal processing and electronic from <a href="http://www.ensea.fr"><i>l'Ecole Nationale Superieur de l'Electronique et de ses Applications (ENSEA)</i></a> in Paris, France. Since my graduation in 2011 I have been working as a developer for companies such as  <a href="http://www.voxler.fr/"> <i>Voxler Games </i></a> or <a href="http://www.arkamys.com/home.php"> <i>Arkamys</i></a> mostly n embeeded audio processing and voice regognition.For the past year I worked at <a href="http://www.ircam.fr/"> <i>L'Institut de Recherche et Coordination Acoustique / Musique (IRCAM) </i></a> as a research engineer and a <i>C++</i> developer. 
+	I am also a musician and a sound passionate. I am currently working on multiple projects that involves technologie and sounds. 		 
+      </p>	
+      
+      <div class="box">
+	<p class="text">
+	  My current work is focused on designing tangible objects and electronic instruments that allows the composition and the improvisation without having to use a computer. I am also interrested with the development of web applications that allows real time interractions.
+	</p>	  	
+      </div>
+      
+      <div class="box">	
+	<p class="text">		      
+	  I am currently looking for employments opportunities in San Francisco and in the bay area, I am eager to discover new technologies and increase the span of skills, and I am very open on the fields of applications. I worship interdisciplinarity and I think that creativity dosen't come from a spontaneous generation.
+	</p>		
+      </div>
+      
+      <div class="box">	
+	<p class="text">	    	
+	  you can also find me <a href="http://www.twitter.com/magnizdat"><i>here</i></a>.
+	  and a small part of my work can be seen <a href="http://www.github.com/FabienCesari"><i>here</i></a>.
+	</p> 	
+      </div>	   
+    </div>        
+    <canvas id="canvas1" width="200" height="200"></canvas>  
+    <script src="js/background.js"></script>    
   </body>
 </html>
+  
