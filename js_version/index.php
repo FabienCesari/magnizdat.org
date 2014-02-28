81736aebf61363958de5445bb98128be118936d7
magnizdat <fabien.cesari@gmail.com>
2014-02-28 11:32:22 -0800

    Not happy with this graphics, last commit before changing

--- js_version/index.php
+++ js_version/index.php
@@ -5,36 +5,35 @@
     <meta charset="utf-8" />
     <meta name="description" content="Fabien Cesari's online CV and portfolio" />
     <link rel="stylesheet" media="all" href="css/style.css" />
-    <!--[if lt IE 9]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->        
+    <script src="js/modernizr.custom.37797.js"></script> 
+    <!-- Grab Google CDN's jQuery. fall back to local if necessary --> 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> 
     <script>!window.jQuery && document.write('<script src="/js/jquery-1.6.1.min.js"><\/script>')</script>
-    <script type="text/javascript" src="js/parralax.js"></script>
+    <script src="js/parallax.js"></script>    
   </head>
   
-  <body>    
-    
+  <body>        
     <div id="wrapper">
       <header id="branding">
 	<h1>Fabien Cesari</h1>
+	<h2> Engineer, Musician, developer, digital craftsman.</h2>
       </header>
       <nav id="primary">
 	<ul>
 	  <li>
-	    <h1>Signal processing engineer - developer</h1>
+	    <h1>Where I come from.</h1>
 	    <a class="work_exp" href="#work_exp">View</a>
 	  </li>
 	  <li>
-	    <h1>Education</h1>
+	    <h1>Where I am.</h1>
 	    <a class="education" href="#education">View</a>
 	  </li>
 	  <li>
-	    <h1>Music</h1>
+	    <h1>Where I am going.</h1>
 	    <a class="music" href="#music">View</a>
 	  </li>
 	  <li>
-	    <h1>Purpose of this website</h1>
+	    <h1>About</h1>
 	    <a class="about" href="#about">View</a>
 	  </li>
 	</ul>
@@ -44,69 +43,90 @@
     <div id="content">
       <article id="work_exp">
 	<header>
-	  <h1>Signal processing engieer - developer</h1>
+	  <h1>Where I come from.</h1>
 	</header>
-	  <p>
-	    
-	  </p>
-	  <nav class="next-prev">
-	    <hr />
-	    <a class="next education" href="#education">Next</a>
-	  </nav>
+	<p>
+	  I am an engineer specialized in signal processing and electronic from <a href="http://www.ensea.fr"><i>l'Ecole Nationale Superieur de l'Electronique et de ses Applications (ENSEA)</i></a> in Paris, France. Since my graduation in 2011 I have been working as a developer for companies such as  <a href="http://www.voxler.fr/"> <i>Voxler Games </i></a> or <a href="http://www.arkamys.com/home.php"> <i>Arkamys</i></a> mostly n embeeded audio processing and voice regognition.For the past year I worked at <a href="http://www.ircam.fr/"> <i>L'Institut de Recherche et Coordination Acoustique / Musique (IRCAM) </i></a> as a research engineer and a <i>C++</i> developer. 
+	</p>
+	<p>
+	  I am also a musician and a sound passionate. I am currently working on multiple projects that involves technologie and sounds. 
+	</p>
+	<nav class="next-prev">
+	  <hr />
+	  <a class="next education" href="#education">Next</a>
+	</nav>
       </article>
-	
-	<article id="education">
-	  <header>
-	    <h1>education</h1>
-	  </header>
-	  <p>	  	    	  </p>	  
-	  <nav class="next-prev">
-	    <a class="prev work_exp" href="#work_exp">Prev</a>
-	    <hr />
-	    <a class="next music" href="#music">Next</a>
-	  </nav>
-	</article>
-	
-	<article id="music">
-	  <header>
-	    <h1>Music</h1>
-	  </header>
-	  <p>	    	  </p>
-	  <nav class="next-prev">
-	    <a class="prev education" href="#education">Prev</a>
-	    <hr />
-	    <a class="next about" href="#about">Next</a>
-	  </nav>
-	</article>
-	
-	<article id="about">
-	  <header>
-	    <h1>About</h1>
-	  </header>
-	  <p>	    	  </p>	  
-	  <nav class="next-prev">
-	    <a class="prev music" href="#music">Prev</a>
-	    <hr />
-	  </nav>
-	</article>
+      
+      <article id="education">
+	<header>
+	  <h1>Where I am</h1>
+	</header>
+	<p>My current work is focused on designing tangible objects and electronic instruments that allows the composition and the improvisation without having to use a computer. I am also interrested with the development of web applications that allows real time interractions.
+	</p>
+	<nav class="next-prev">
+	  <a class="prev work_exp" href="#work_exp">Prev</a>
+	  <hr />
+	  <a class="next music" href="#music">Next</a>
+	</nav>
+      </article>
+      
+      <article id="music">
+	<header>
+	  <h1>Where I am going.</h1>
+	</header>
+	<p>
+	  I am currently looking for employments opportunities in San Francisco and in the bay area, I am eager to discover new technologies and increase the span of skills, and I am very open on the fields of applications. I worship interdisciplinarity and I think that creativity dosen't come from a spontaneous generation.
+	</p>
+	<nav class="next-prev">
+	  <a class="prev education" href="#education">Prev</a>
+	  <hr />
+	  <a class="next about" href="#about">Next</a>
+	</nav>
+      </article>
+      
+      <article id="about">
+	<header>
+	  <h1>About</h1>	 
+	</header>
+	<p>	    	
+	  you can also find me <a href="http://www.twitter.com/magnizdat"><i>here</i></a>.
+	</p> 
+	<p>
+	  and a small part of my work can be seen <a href="http://www.github.com/FabienCesari"><i>here</i></a>.
+	</p>	  
+	<nav class="next-prev">
+	  <a class="prev music" href="#music">Prev</a>
+	  <hr />
+	</nav>
+      </article>
+    </div>
+  </div>
+  
+        <!-- Parallax foreground -->
+      <div id="parallax-bg3">
+	<!-- <img id="bg3-1" src="im/balloon.png" width="529" height="757" alt="Montgolfier hot air balloon"/> -->
+	<!-- <img id="bg3-2" src="im/balloon2.png" width="603" height="583" alt="Frameless parachute"/> -->
+	<!-- <img id="bg3-3" src="im/balloon3.png" width="446" height="713" alt="Blanchard's air balloon"/> -->
+	<!-- <img id="bg3-4" src="im/ground.png" width="1104" height="684" alt="Landscape with trees and cows"/> -->
       </div>
-    </div>
-    
-    <!-- Parralax for background -->
-    <div id="parralax_bg1">      
-      <img id="bg1-1" src="im/background_1.png" alt="cloud"/>
-      <img id="bg1-2" src="im/background_2.png" alt="cloud"/>
-      <img id="bg1-3" src="im/background_3.png" alt="cloud"/>
-    </div>
-    
-    <!-- Parralax for midground -->
-    <div id="parralax_bg2">      
-    </div>
-
-    <!-- Parralax for foreground -->
-    <div id="parralax_bg3">      
-    </div>
-    
-
+      
+      <!-- Parallax  midground clouds -->
+      <div id="parallax-bg2">
+	<img id="bg2-1" src="im/cloud-lg1.png" alt="cloud"/>
+	<img id="bg2-2" src="im/cloud-lg1.png" alt="cloud"/>
+	<img id="bg2-3" src="im/cloud-lg1.png" alt="cloud"/>
+	<img id="bg2-4" src="im/cloud-lg1.png" alt="cloud"/>
+	<img id="bg2-5" src="im/cloud-lg1.png" alt="cloud"/>
+      </div>
+      
+      <!-- Parallax  background clouds -->
+      <div id="parallax-bg1">
+	<img id="bg1-1" src="im/cloud-lg2.png" alt="cloud"/>
+	<img id="bg1-2" src="im/cloud-lg2.png" alt="cloud"/>
+	<img id="bg1-3" src="im/cloud-lg2.png" alt="cloud"/>
+	<img id="bg1-4" src="im/cloud-lg2.png" alt="cloud"/>   
+      </div>			
+      
+    </div>		          
   </body>
 </html>
